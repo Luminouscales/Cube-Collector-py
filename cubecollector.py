580967789ef423b0edb5446b807894aa0d2f2ae7
Luminouscales <82906871+Luminouscales@users.noreply.github.com>
2024-05-05 17:16:14 +0200

    Added saving for inventory and registry

--- cubecollector.py
+++ cubecollector.py
@@ -2,6 +2,7 @@ import os, sys, random, time, atexit
 
 selfpath = os.path.dirname(sys.argv[0])
 inventorypath = selfpath + "/inventory.txt"
+registrypath = selfpath + "/registry.txt"
 
 # Read the inventory and output it into the "inventory" var
 inventory = []
@@ -11,12 +12,41 @@ if os.path.exists(inventorypath):
             row = line.strip().split('\t') # What?
             inventory.append( row )
 
+# Load registry. The registry is a list of all ever unboxed cubes
+registry = []
+if os.path.exists( registrypath ):
+    with open( registrypath, 'r' ) as file:
+        for line in file:
+            row = line.strip().split('\t')
+            registry.append( row )
+
 # Take all cubes from inventory var and save it as inventory.txt
 def saveinv():
     with open( inventorypath, 'w' ) as file:
         for row in inventory:
             row_str = '\t'.join(map(str, row)) # What the fuck is map() ?
             file.write( row_str + '\n')
+
+# Take all cubes from reg and save it as registry.txt
+def savereg():
+    with open( registrypath, 'w' ) as file:
+        for row in registry:
+            row_str = '\t'.join(map(str, row)) # What the fuck is map() ?
+            file.write( row_str + '\n')
+
+# Add cube to registry
+def addreg( cube ):
+    for row in registry:
+        # If cube already in registry add one more to count and end function
+        if row[0] == cube:
+            row[1] = int( row[1] ) + 1
+            savereg()
+            return 
+    # If new, add to reg
+    # First one is name, second one is number of times found, second is unique ID.
+    # Unique ID is counted by adding one to last cube's ID. Let's just hope it's sorted correctly.
+    registry.append( [ cube, '1', int( registry[len(registry) - 1][2] ) + 1 ] )
+    savereg()
 
 # Import list of prefixes
 with open( selfpath + "/prefixes.txt", 'r' ) as file:
@@ -53,7 +83,13 @@ def addcube( cube ):
         if row[0] == cube:
             row[1] = int( row[1] ) + 1
             print( "You got a " + cube + "!" )
+            saveinv()
+            # Add to reg
+            addreg( cube )
             return 
     # If new then add the cube to inventory. The 1 is the amount of new cubes. So, just one.
     inventory.append( [cube, '1'] )
+    saveinv()
+    # Add to reg
+    addreg( cube )
     print( "You got a brand new " + cube + "!" )
